734b6bd4db0e979b26625d8c191e7eb48e7163cb
s1ashz <francisco.filipe@inseinc.com>
2019-04-04 09:35:55 +0100

    reverted account table

--- src/main/java/com/polo/rest/polo/entity/Account.java
+++ src/main/java/com/polo/rest/polo/entity/Account.java
@@ -29,7 +29,6 @@ public class Account
     private boolean exam;
     private boolean enrolled;
     private String email;
-    private String leeel;
     
     @Column(columnDefinition="TEXT")
     private String token;
@@ -136,21 +135,12 @@ public class Account
 	public void setToken(String token) {
 		this.token = token;
 	}
-	
-    public String getLeeel() {
-        return leeel;
-    }
-
-    public void setLeeel( String leeel ) {
-        this.leeel = leeel;
-    }
-
-    @Override
-    public String toString() {
-        return "Account [id=" + id + ", cardId=" + cardId + ", name=" + name + ", postalCode=" + postalCode + ", address=" + address
-                + ", birthday=" + birthday + ", gender=" + gender + ", mobileNumber=" + mobileNumber + ", level=" + level
-                + ", size=" + size + ", cc=" + cc + ", exam=" + exam + ", enrolled=" + enrolled + ", email=" + email + ", leeel="
-                + leeel + ", token=" + token + ", observations=" + observations + "]";
-    }
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", cardId=" + cardId + ", name=" + name + ", postalCode=" + postalCode
+				+ ", address=" + address + ", birthday=" + birthday + ", gender=" + gender + ", mobileNumber="
+				+ mobileNumber + ", level=" + level + ", size=" + size + ", observations=" + observations + ", cc=" + cc
+				+ ", exam=" + exam + ", enrolled=" + enrolled + ", email=" + email + ", token=" + token + "]";
+	}
     
 }
